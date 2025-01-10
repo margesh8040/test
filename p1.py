42a60451d62db8d31670408ffe36b5e705abd9bd
margesh8040 <polaramargesh6400@gmail.com>
2025-01-10 09:04:15 +0530

    BPv3

--- p1.py
+++ p1.py
@@ -3,3 +3,5 @@ number = 8040
 print("The number is:", number)
 
 print("The number is:", number, "and the square of the number is:", number*number)
+
+print("The number is: %d and the square of the number is: %d" % (number, number*number))
